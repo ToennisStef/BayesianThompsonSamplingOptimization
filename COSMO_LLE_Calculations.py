0c26c533c682c7f0ce9aefa2319466a535657e3b
ToennisStef <stefan.toennis@gmx.de>
2025-06-26 16:19:52 +0200

    Add COSMO_LLE_Calculations script and input file structure for liquid-liquid equilibrium calculations
    - Implemented a new script `COSMO_LLE_Calculations.py` for performing liquid-liquid equilibrium calculations using COSMOtherm.
    - Added logging functionality to track the execution process.
    - Included functions to generate input files for vapor pressure calculations.
    - Created directory structure for input files with `.gitkeep` files to maintain empty directories for screening data.

---
+++ COSMO_LLE_Calculations.py
@@ -0,0 +1,94 @@
+# Exemplary calcualtion of liquid-liquid equilibrium calculation using COSMOtherm
+# Calculation at different temperature and (water-lacticacid)composition levels
+# Phase 1 is denoted as "p1" and Phase 2 as "p2"
+
+
+# --- Imports ---
+from COSMOtherm.Configfiles import Config
+from COSMOtherm.Configfiles import COSMOthermConfig  # Importing Config_ for the input file generation
+from COSMOtherm.src.COSMOtherm_functions.Inputfile_Generation import gen_TBOIL_inp_file, gen_binaryLLE_inp_file, gen_binaryActivity_inp_file, gen_PVAP_inp_file
+from COSMOtherm.src.COSMOtherm_functions.Run_COSMOtherm_Calculations import run_COSMOtherm_calculations
+import logging
+import pandas as pd
+import os
+# Ensure the logs directory exists
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+# Configure logging in the main file
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler(os.path.join(log_dir, "COSMO_LLE_Calculation.log")),
+        logging.StreamHandler()
+    ]
+)
+
+
+# --- Main Execution ---
+if __name__ == "__main__":
+    
+    logging.info("Starting new COSMOtherm LLE calculation run.")
+        
+    
+    # Load the solvents data from the CSV file 
+    solvents = pd.read_csv(Config.solvents_fullpath)
+    
+    Server = 'TIGER'  # or 'TIGER2'
+    
+    for solvent in solvents['COSMO_name'].unique():
+        
+        # file = gen_TBOIL_inp_file(
+        #     solvent=solvent, 
+        #     ctd_file=COSMOthermConfig.TIGER['ctd_file'], 
+        #     cdir=COSMOthermConfig.TIGER['cdir'], 
+        #     ldir=COSMOthermConfig.TIGER['ldir'], 
+        #     odir=COSMOthermConfig.TIGER['odir'], 
+        #     fdir=COSMOthermConfig.TIGER['fdir'],
+        #     overwrite=True
+        #     )
+        
+        # solvent_ = "ethanol"
+        
+        # file = gen_binaryLLE_inp_file(
+        #     solvent=solvent, 
+        #     ctd_file=COSMOthermConfig.TIGER['ctd_file'], 
+        #     cdir=COSMOthermConfig.TIGER['cdir'], 
+        #     ldir=COSMOthermConfig.TIGER['ldir'], 
+        #     odir=COSMOthermConfig.TIGER['odir'], 
+        #     fdir=COSMOthermConfig.TIGER['fdir'],
+        #     overwrite=True
+        #     )
+        
+        # file = gen_binaryActivity_inp_file(
+        #     solvent=solvent, 
+        #     ctd_file=COSMOthermConfig.TIGER['ctd_file'], 
+        #     cdir=COSMOthermConfig.TIGER['cdir'], 
+        #     ldir=COSMOthermConfig.TIGER['ldir'], 
+        #     odir=COSMOthermConfig.TIGER['odir'], 
+        #     fdir=COSMOthermConfig.TIGER['fdir'],
+        #     overwrite=True
+        #     )
+        
+        file =  gen_PVAP_inp_file(
+            solvent=solvent, 
+            t_start=25,
+            t_end=100,
+            t_step=5,
+            ctd_file=COSMOthermConfig.TIGER['ctd_file'], 
+            cdir=COSMOthermConfig.TIGER['cdir'], 
+            ldir=COSMOthermConfig.TIGER['ldir'], 
+            odir=COSMOthermConfig.TIGER['odir'], 
+            fdir=COSMOthermConfig.TIGER['fdir'],
+            overwrite=True
+            )
+        
+        
+        
+        run_COSMOtherm_calculations(
+            COSMOtherm_exe_fullpath=COSMOthermConfig.TIGER['exe_fullpath'],
+            files=[file['fullpath']]
+        )
+        
+        break
--- COSMOtherm/src/COSMOtherm_functions/Inputfile_Generation.py
+++ COSMOtherm/src/COSMOtherm_functions/Inputfile_Generation.py
@@ -220,3 +220,285 @@ FDIR="{fdir}" vpfile CTAB WCONF AUTOC                                 # Global c
         "filename": file_name,
         "folder": os.path.abspath(inputfiles_folder)
     }
+    
+def gen_TBOIL_inp_file(
+    solvent:str, 
+    ctd_file:str, 
+    cdir:str, 
+    ldir:str, 
+    odir:str, 
+    fdir:str,
+    file_name:str = None,  
+    inputfiles_folder:str =".",
+    overwrite:bool = False
+    )->None:
+    """
+    Generates an input file for TBOIL calculations in COSMOtherm for a given solvent.
+    
+    Parameters:
+        solvent (str): Name of the solvent.
+        ctd_file (str): Path to the CT-Data file.
+        cdir (str): Directory for the CT-data/parameterization file.
+        ldir (str): Directory for the license file.
+        odir (str): Directory for the output files.
+        fdir (str): Directory for the compound data files.
+        file_name (str, optional): Name of the input file. If None, a default name will be generated.
+        inputfiles_folder (str, optional): Directory where the input file will be saved. Default is the current directory.
+        overwrite (bool, optional): If True, overwrite existing files. Default is False.
+
+    Returns:
+        str: The full path of the generated input file.
+        str: The name of the generated input file.
+    """
+    
+    # Ensure the output directory exists
+    os.makedirs(inputfiles_folder, exist_ok=True)
+    logger.debug(f"Ensured the directory '{inputfiles_folder}' exists.")
+    
+    if file_name is None:
+        file_name = f"TBOIL_{solvent}.inp"
+    file_fullpath = os.path.join(inputfiles_folder, file_name)
+    
+    # Define content
+    content = f"""ctd={ctd_file} CDIR="{cdir}" LDIR="{ldir}" odir="{odir}" # Global command line
+FDIR="{fdir}" efile UNIT=SI AUTOC                                 # Global command line
+!! TBOIL calculation for a single solvent                          # Comment line
+"""
+    content += f"f = {solvent}\n"
+    content += f"x={{1}} pvap=1013.2 tk=300.0 "
+    
+    if os.path.exists(file_fullpath):
+        logger.warning(f"File already exists: {file_fullpath}")
+        if not overwrite:
+            logger.info(f"Skipping inputfile generation for: '{file_name}'")
+            logger.info("The file was not overwritten. Returning the existing file path and name.")
+            return {
+                "fullpath": os.path.abspath(file_fullpath),
+                "filename": file_name,
+                "folder": os.path.abspath(inputfiles_folder)
+            }
+            
+    with open(file_fullpath, "w") as file:
+        logger.debug(f"Generating inputfile: '{file_fullpath}'")
+        file.write(content)
+        logger.info(f"Input file '{file_fullpath}' has been created successfully. Returning the file path and name.")
+    
+    return {
+        "fullpath": os.path.abspath(file_fullpath),
+        "filename": file_name,
+        "folder": os.path.abspath(inputfiles_folder)
+    }
+    
+    
+def gen_binaryLLE_inp_file(
+    solvent:str, 
+    ctd_file:str, 
+    cdir:str, 
+    ldir:str, 
+    odir:str, 
+    fdir:str,
+    file_name:str = None,  
+    inputfiles_folder:str =".",
+    overwrite:bool = False
+    )->None:
+    """
+    Generates an input file for TBOIL calculations in COSMOtherm for a given solvent.
+    
+    Parameters:
+        solvent (str): Name of the solvent.
+        ctd_file (str): Path to the CT-Data file.
+        cdir (str): Directory for the CT-data/parameterization file.
+        ldir (str): Directory for the license file.
+        odir (str): Directory for the output files.
+        fdir (str): Directory for the compound data files.
+        file_name (str, optional): Name of the input file. If None, a default name will be generated.
+        inputfiles_folder (str, optional): Directory where the input file will be saved. Default is the current directory.
+        overwrite (bool, optional): If True, overwrite existing files. Default is False.
+
+    Returns:
+        str: The full path of the generated input file.
+        str: The name of the generated input file.
+    """
+    
+    # Ensure the output directory exists
+    os.makedirs(inputfiles_folder, exist_ok=True)
+    logger.debug(f"Ensured the directory '{inputfiles_folder}' exists.")
+    
+    if file_name is None:
+        file_name = f"binaryLLE_h2o_{solvent}.inp"
+    file_fullpath = os.path.join(inputfiles_folder, file_name)
+    
+    # Define content
+    content = f"""ctd={ctd_file} CDIR="{cdir}" LDIR="{ldir}" odir="{odir}" # Global command line
+FDIR="{fdir}" efile UNIT=SI AUTOC nomix                                # Global command line
+! Binary phase VLE/LLE diagram computation with phase separation                  # Comment line
+"""
+    content += f"f = h2o\n"  # Water is always the first component in binary LLE
+    content += f"f = {solvent}\n"
+    content += f"tc=25 tc2=200 tstepsize=10 BINARY LLE NRTL                                                            # Automatic binary computation and LLE search"
+    
+    if os.path.exists(file_fullpath):
+        logger.warning(f"File already exists: {file_fullpath}")
+        if not overwrite:
+            logger.info(f"Skipping inputfile generation for: '{file_name}'")
+            logger.info("The file was not overwritten. Returning the existing file path and name.")
+            return {
+                "fullpath": os.path.abspath(file_fullpath),
+                "filename": file_name,
+                "folder": os.path.abspath(inputfiles_folder)
+            }
+            
+    with open(file_fullpath, "w") as file:
+        logger.debug(f"Generating inputfile: '{file_fullpath}'")
+        file.write(content)
+        logger.info(f"Input file '{file_fullpath}' has been created successfully. Returning the file path and name.")
+    
+    return {
+        "fullpath": os.path.abspath(file_fullpath),
+        "filename": file_name,
+        "folder": os.path.abspath(inputfiles_folder)
+    }
+    
+    
+def gen_binaryActivity_inp_file(
+    solvent:str, 
+    ctd_file:str, 
+    cdir:str, 
+    ldir:str, 
+    odir:str, 
+    fdir:str,
+    file_name:str = None,  
+    inputfiles_folder:str =".",
+    overwrite:bool = False
+    )->None:
+    """
+    Generates an input file for TBOIL calculations in COSMOtherm for a given solvent.
+    
+    Parameters:
+        solvent (str): Name of the solvent.
+        ctd_file (str): Path to the CT-Data file.
+        cdir (str): Directory for the CT-data/parameterization file.
+        ldir (str): Directory for the license file.
+        odir (str): Directory for the output files.
+        fdir (str): Directory for the compound data files.
+        file_name (str, optional): Name of the input file. If None, a default name will be generated.
+        inputfiles_folder (str, optional): Directory where the input file will be saved. Default is the current directory.
+        overwrite (bool, optional): If True, overwrite existing files. Default is False.
+
+    Returns:
+        str: The full path of the generated input file.
+        str: The name of the generated input file.
+    """
+    
+    # Ensure the output directory exists
+    os.makedirs(inputfiles_folder, exist_ok=True)
+    logger.debug(f"Ensured the directory '{inputfiles_folder}' exists.")
+    
+    if file_name is None:
+        file_name = f"binaryactivity_h2o_{solvent}.inp"
+    file_fullpath = os.path.join(inputfiles_folder, file_name)
+    
+    # Define content
+    content = f"""ctd={ctd_file} CDIR="{cdir}" LDIR="{ldir}" odir="{odir}" # Global command line
+FDIR="{fdir}" AUTOC                                 # Global command line
+!! Activity Coefficient computation                                               # Comment line
+"""
+    content += f"f = h2o\n"  # Water is always the first component in binary LLE
+    content += f"f = {solvent}\n"
+    content += f"tc=25 gamma xg={{{"0.24 0.76"}}}"                                                              # Automatic binary computation and LLE search"
+    
+    if os.path.exists(file_fullpath):
+        logger.warning(f"File already exists: {file_fullpath}")
+        if not overwrite:
+            logger.info(f"Skipping inputfile generation for: '{file_name}'")
+            logger.info("The file was not overwritten. Returning the existing file path and name.")
+            return {
+                "fullpath": os.path.abspath(file_fullpath),
+                "filename": file_name,
+                "folder": os.path.abspath(inputfiles_folder)
+            }
+            
+    with open(file_fullpath, "w") as file:
+        logger.debug(f"Generating inputfile: '{file_fullpath}'")
+        file.write(content)
+        logger.info(f"Input file '{file_fullpath}' has been created successfully. Returning the file path and name.")
+    
+    return {
+        "fullpath": os.path.abspath(file_fullpath),
+        "filename": file_name,
+        "folder": os.path.abspath(inputfiles_folder)
+    }
+    
+
+def gen_PVAP_inp_file(
+    solvent:str, 
+    t_start:float,
+    t_end:float,    
+    t_steps:int,
+    ctd_file:str, 
+    cdir:str, 
+    ldir:str, 
+    odir:str, 
+    fdir:str,
+    file_name:str = None,  
+    inputfiles_folder:str =".",
+    overwrite:bool = False
+    )->None:
+    """
+    Generates an input file for TVAP (Vapour pressure calculation) calculations in COSMOtherm for a given solvent.
+    Vapour pressure calculations are executed at different temperatures. 
+    When more then three temperatures are specified, the Antoine parameters are fitted to the data.
+    
+    Parameters:
+        solvent (str): Name of the solvent.
+        ctd_file (str): Path to the CT-Data file.
+        cdir (str): Directory for the CT-data/parameterization file.
+        ldir (str): Directory for the license file.
+        odir (str): Directory for the output files.
+        fdir (str): Directory for the compound data files.
+        file_name (str, optional): Name of the input file. If None, a default name will be generated.
+        inputfiles_folder (str, optional): Directory where the input file will be saved. Default is the current directory.
+        overwrite (bool, optional): If True, overwrite existing files. Default is False.
+
+    Returns:
+        str: The full path of the generated input file.
+        str: The name of the generated input file.
+    """
+    
+    # Ensure the output directory exists
+    os.makedirs(inputfiles_folder, exist_ok=True)
+    logger.debug(f"Ensured the directory '{inputfiles_folder}' exists.")
+    
+    if file_name is None:
+        file_name = f"PVAP_{solvent}.inp"
+    file_fullpath = os.path.join(inputfiles_folder, file_name)
+    
+    # Define content
+    content = f"""ctd={ctd_file} CDIR="{cdir}" LDIR="{ldir}" odir="{odir}" # Global command line
+FDIR="{fdir}" AUTOC UNIT=SI                                # Global command line
+!  Automatic computation of vapor pressure curve p_vap(T)                                          # Comment line
+"""
+    content += f"f = {solvent}\n"
+    content += f"x={{1}} pvap tc={t_start} tc2={t_end} tstep={t_steps}"                                                              # Automatic binary computation and LLE search"
+    
+    if os.path.exists(file_fullpath):
+        logger.warning(f"File already exists: {file_fullpath}")
+        if not overwrite:
+            logger.info(f"Skipping inputfile generation for: '{file_name}'")
+            logger.info("The file was not overwritten. Returning the existing file path and name.")
+            return {
+                "fullpath": os.path.abspath(file_fullpath),
+                "filename": file_name,
+                "folder": os.path.abspath(inputfiles_folder)
+            }
+            
+    with open(file_fullpath, "w") as file:
+        logger.debug(f"Generating inputfile: '{file_fullpath}'")
+        file.write(content)
+        logger.info(f"Input file '{file_fullpath}' has been created successfully. Returning the file path and name.")
+    
+    return {
+        "fullpath": os.path.abspath(file_fullpath),
+        "filename": file_name,
+        "folder": os.path.abspath(inputfiles_folder)
+    }
